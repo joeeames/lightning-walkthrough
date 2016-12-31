9fd08d928c29398dce908501e7e8bf3c999faabe
Joe Eames <joeeames@gmail.com>
2016-12-30 22:15:53 -0700

    switched over to ts, removed build dir

--- public/admin/userDetails.js
+++ public/admin/userDetails.js
@@ -1,12 +1,12 @@
-angular.module('app').component('userDetails', { 
-  templateUrl: '/admin/userDetails.html',
-  bindings: {
-    allUsers: '='
-  },
-  controller: function($routeParams) {
-    
-    this.user = this.allUsers.find(function(user) {
-      return user.id === parseInt($routeParams.id);
-    })
-  }
-})
+angular.module('app').component('userDetails', {
+    templateUrl: '/admin/userDetails.html',
+    bindings: {
+        allUsers: '='
+    },
+    controller: function ($routeParams) {
+        this.user = this.allUsers.find(function (user) {
+            return user.id === parseInt($routeParams.id);
+        });
+    }
+});
+//# sourceMappingURL=userDetails.js.map
